1ca553e57e98be55a47f11e99ddf29b7dde1914a
Nuel <nueljoel34@gmail.com>
2020-09-24 14:32:03 +0800

    updated blog/views, blog/templates, flr/settings, flr/templates

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse("<h1>It works</h1>")
+	return render(request, 'blog/index.html')
--- flr/settings.py
+++ flr/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'flr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'blog/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'flr/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
