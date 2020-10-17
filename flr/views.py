8c634d1f80e2f0d78bc0e7d8dd752e9e16060c31
nueldeli <nueljoel34@gmail.com>
2020-10-18 01:31:13 +0800

    added blog on homepage

--- flr/views.py
+++ flr/views.py
@@ -3,15 +3,12 @@ from django.shortcuts import render
 from blog.models import Post
 
 def home(request):
-	return render(request, 'home.html')
+	post_list = Post.objects.all().order_by('-date_created')[:3]
+	context = {'post_list':post_list}
+	return render(request, 'home.html', context)
 
 def about(request):
 	return render(request, 'about.html')
 
 def organization(request):
 	return render(request, 'organization.html')
-
-def all_posts(request):
-	post_list = Post.objects.all().order_by('date_created')[:3]
-	context = {'post_list' : post_list}
-	return render(request, '_include/latest_stories.html', context)
