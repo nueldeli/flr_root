f241fc33124a421b402c516b84e43bf801540fc6
Nuel <nueljoel34@gmail.com>
2020-11-13 17:06:16 +0800

    data-app, analytics overview - progressing

--- data/views.py
+++ data/views.py
@@ -4,31 +4,45 @@ from .models import TreeSpeciesData
 from .forms import AddTreeForm, UpdateTreeForm
 from django.urls import reverse_lazy
 
-all_tree_species = TreeSpeciesData.objects.all()
-
-niah_list = all_tree_species.filter(nursery__icontains='Niah')
-sabal_list = all_tree_species.filter(nursery__icontains='Sabal')
-semenggoh_list = all_tree_species.filter(nursery__icontains='IFRC')
-
-niah_count = niah_list.count()
-sabal_count = sabal_list.count()
-semenggoh_count = semenggoh_list.count()
-
 # Create your views here.
 def data_index(request):
 	return render(request, 'data/data_index.html')
 
+#---Nursery views ---
 def nursery_index(request):
+	all_tree_species = TreeSpeciesData.objects.all()
+	niah_count = all_tree_species.filter(nursery__icontains='Niah').count()
+	sabal_count = all_tree_species.filter(nursery__icontains='Sabal').count()
+	semenggoh_count = all_tree_species.filter(nursery__icontains='IFRC').count()
 	return render(request, 'data/nursery.html', {'niah_count':niah_count, 'sabal_count':sabal_count, 'semenggoh_count':semenggoh_count})
 
 def sabal_species(request):
+	all_tree_species = TreeSpeciesData.objects.all()
+	sabal_list = all_tree_species.filter(nursery__icontains='Sabal')
 	return render(request, 'data/_include/sabal_species.html', {'sabal_list':sabal_list})
 
 def semenggoh_species(request):
+	all_tree_species = TreeSpeciesData.objects.all()
+	semenggoh_list = all_tree_species.filter(nursery__icontains='Semenggoh')
 	return render(request, 'data/_include/semenggoh_species.html', {'semenggoh_list':semenggoh_list})
 
 def niah_species(request):
+	all_tree_species = TreeSpeciesData.objects.all()
+	niah_list = all_tree_species.filter(nursery__icontains='Niah')
 	return render(request, 'data/_include/niah_species.html', {'niah_list':niah_list})
+#--- end of nursery views --
+
+
+#---chart / analytics overview---
+def pie_chart(request):
+	all_tree_species = TreeSpeciesData.objects.all()
+	niah_count = all_tree_species.filter(nursery__icontains='Niah').count()
+	sabal_count = all_tree_species.filter(nursery__icontains='Sabal').count()
+	semenggoh_count = all_tree_species.filter(nursery__icontains='IFRC').count()
+	label = ['Sabal FLR Centre', 'Niah Forest Research Station', 'IFRC Semenggoh']
+	data = [sabal_count, niah_count, semenggoh_count]
+	return render(request, 'data/data_index.html', {'label':label, 'data':data})
+#---end of chart / analytics overview ---
 
 class TreeSpeciesView(ListView):
 	model = TreeSpeciesData
