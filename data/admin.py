a0c5c22b363bf995ad563a52cb4136d33d566c2b
Nuel <nueljoel34@gmail.com>
2020-11-16 17:03:41 +0800

    updated data + planting record model, added planting app

--- data/admin.py
+++ data/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import TreeSpeciesData
+from .models import TreeSpeciesData, PlantingRecord
 
 # Register your models here.
 admin.site.register(TreeSpeciesData)
+admin.site.register(PlantingRecord)
--- data/models.py
+++ data/models.py
@@ -26,3 +26,25 @@ class TreeSpeciesData(models.Model):
 
 	def get_absolute_url(self):
 		return reverse('data')
+
+#Model on planting record
+class PlantingRecord(models.Model):
+	date_planted = models.DateTimeField()
+	programme_name = models.CharField(max_length=250)
+	programme_img = models.ImageField('Programme Image', null=True, blank=True, upload_to='programme_img/')
+	location = models.CharField(max_length=250)
+	region = models.CharField(max_length=250)
+	area = models.FloatField()
+	species = models.CharField(max_length=255)
+	participants = models.CharField(max_length=250)
+
+	def __str__(self):
+		return self.programme_name
+
+	def get_programme_img(self):
+		if self.programme_img and hasattr(self.programme_img, 'url') :
+			return self.programme_img.url
+		return ImageFieldFile(instance=None, field=FileField(), name='/media/programme_img/programme_img_default.png')
+
+	def get_absolute_url(self):
+		return reverse('data')
